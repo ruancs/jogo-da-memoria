a14f4a5a90f36687d1df50ab4c755208ea79a7d2
ruancs <ruan.cs@hotmail.com>
2022-02-18 19:12:47 -0300

    adicionado logica do jogo e favicon

--- scripts.js
+++ scripts.js
@@ -1,7 +1,58 @@
 const cards = document.querySelectorAll('.memory-card');
+var sound = document.getElementById("flipin");
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
 
 function flipCard(){
-    this.classList.toggle('flip');
+    if (lockBoard) return;
+    if(this === firstCard) return;
+    this.classList.add('flip');
+    sound.play(); 
+
+    if (!hasFlippedCard){
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+
+    secondCard = this;
+    // hasFlippedCard = false;
+
+    checkforMatch();
+
+}
+
+function checkforMatch(){
+    if (firstCard.dataset.pokemon === secondCard.dataset.pokemon){
+        disableCards();
+        return;
+    }
+
+    unflipCards();
+}
+
+function disableCards(){
+    firstCard.removeEventListener('click',flipCard);
+    secondCard.removeEventListener('click', flipCard);
+    resetBoard();
+}
+
+function unflipCards(){
+    lockBoard = true;
+
+    setTimeout(() =>{
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+
+        //lockBoard = false;
+        resetBoard();
+    }, 1500);
+}
+
+function resetBoard(){
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
